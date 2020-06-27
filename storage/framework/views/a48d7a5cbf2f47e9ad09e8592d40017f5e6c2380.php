9d68786f37815c39e246b50273590aa8c9862373
LongBOI-nekoshka <67494206+LongBOI-nekoshka@users.noreply.github.com>
2020-06-27 13:10:12 +0800

    Add files via upload

---
+++ storage/framework/views/a48d7a5cbf2f47e9ad09e8592d40017f5e6c2380.php
@@ -0,0 +1,7 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <h1>About</h1>
+    <p><?php echo e($aboutApp); ?></p>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.appNAuth', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\wamp64\www\nmstrackersystem\resources\views/pages/about.blade.php ENDPATH**/ ?>
